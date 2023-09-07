97d66dace81dd1d4caa4d9a6e4ce27316ce67de9
Veronika Aniskova <v9037@yandex.ru>
2023-09-07 16:30:09 +0300

    before level-3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import { ProtectedRoute } from '../ProtectedRoute/ProtectedRoute';
+
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
@@ -13,7 +16,7 @@ import Login from '../Login/Login';
 import ErrorPage from '../ErrorPage/ErrorPage';
 
 function App() {
-
+  const [currentUser, setCurrentUser] = useState({});
   const [isLogged, setIsLogged] = useState(false);
   const { pathname } = useLocation();
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -40,22 +43,25 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {(pathname === '/' || pathname === '/movies' || pathname === '/saved-movies' || pathname === '/profile') && <Header isLogged={isLogged} onBurgerClick={handlePopupOpen} />}
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/movies" element={<Movies isSaved={false} />} />
-        <Route path="/saved-movies" element={<Movies isSaved={true} />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Register serverError={serverError} />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      {(pathname === '/' || pathname === '/movies' || pathname === '/saved-movies') && <Footer />}
-      <Popup
-        isOpen={isPopupOpen}
-        onClose={closePopup} />
-    </div >
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="App">
+        {(pathname === '/' || pathname === '/movies' || pathname === '/saved-movies' || pathname === '/profile') && <Header isLogged={isLogged} onBurgerClick={handlePopupOpen} />}
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <ProtectedRoute path="/movies" isLogged={isLogged} isSaved={false} element={Movies} />
+          {/* <Route path="/movies" element={<Movies isSaved={false} />} /> */}
+          <Route path="/saved-movies" element={<Movies isSaved={true} />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Register serverError={serverError} />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        {(pathname === '/' || pathname === '/movies' || pathname === '/saved-movies') && <Footer />}
+        <Popup
+          isOpen={isPopupOpen}
+          onClose={closePopup} />
+      </div >
+    </CurrentUserContext.Provider>
   );
 }
 
